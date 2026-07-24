33c9a7c3991f15230432135211d2a03ae681bf19
DESKTOP-PLMAB5D\이효주 <julie08051@naver.com>
2026-07-24 17:31:40 +0900

    Firmware Update v31 + Partition v2

--- HAS1_itembox/HAS1_itembox.ino
+++ HAS1_itembox/HAS1_itembox.ino
@@ -1,5 +1,5 @@
-#define FIRMWARE_VER  30
-#define PARTITION_VER  1
+#define FIRMWARE_VER 31
+#define PARTITION_VER 2
 #include "HAS1_itembox.h"
 
 // Runnable 플래그 갱신 — loop() 가장 먼저 호출. 이 순간의 millis()를 기준으로 통일.
